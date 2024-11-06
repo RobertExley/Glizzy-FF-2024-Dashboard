b6ad75ce74cfecd7f22ee6f30d11d9176b180279
Gautam Maybhate <gautam.maybhate@gmail.com>
2024-11-05 23:10:54 -0800

    data collector outline

---
+++ data_collector/data_collector.py
@@ -0,0 +1,40 @@
+import requests
+
+LEAGUE_ID = '1124814690363400192'
+
+REQUEST_HEADERS = {
+    'Date': 'Wed, 06 Nov 2024 07:02:49 GMT',
+    'Content-Type': 'application/json; charset=utf-8',
+    'Transfer-Encoding': 'chunked',
+    'Connection': 'keep-alive',
+    'access-control-allow-credentials': 'true',
+    'access-control-allow-origin': '*',
+    'access-control-expose-headers': 'etag,date',
+    'cache-control': 'public, s-maxage=300',
+    'x-request-id': 'ee7c57a0d897f20600b1b4e9e1654a4c',
+    'Strict-Transport-Security': 'max-age=15724800; includeSubDomains',
+    'CF-Cache-Status': 'MISS',
+    'Last-Modified': 'Wed, 06 Nov 2024 07:02:49 GMT',
+    'Vary': 'Accept-Encoding',
+    'Server': 'cloudflare',
+    'CF-RAY': '8de338618f2c4313-EWR',
+    'Content-Encoding': 'gzip',
+}
+
+class SleeperDataCollector:
+
+    def __init__(self):
+        pass
+
+    def get_past_weekly_matchups():
+        pass
+
+    def get_future_matchups():
+        pass
+
+    def get_weekly_scores():
+        pass
+
+    def get_wins():
+        pass
+
